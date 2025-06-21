59221b26a18c915dd144dc7c4f4718fa5cbe5ca7
ds-oliver <77216918+ds-oliver@users.noreply.github.com>
2025-06-21 15:46:05 -0700

    Add Google site verification meta tag to layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -32,6 +32,7 @@ export default function RootLayout({
       <head>
         <meta name="darkreader-lock" />
         <meta name="color-scheme" content="light dark" />
+        <meta name="google-site-verification" content="DAVZzr8odYxXMRWlypC3tx_KQrwW7qbNflmNFbucSbo" />
       </head>
       <body
         className={`${poppins.variable} ${inter.variable} antialiased`}
